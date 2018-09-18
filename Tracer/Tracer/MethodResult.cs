ec7be26ccb383effbd379c9b8597273cfe2c0a56
andredance <andrey99.09@gmail.com>
2018-09-18 14:39:43 +0300

    implemented MethodResult class

--- Tracer/Tracer/MethodResult.cs
+++ Tracer/Tracer/MethodResult.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tracer
 {
-    class MethodResult
+    internal class MethodResult
     {
+        private List<MethodResult> innerMethods;
+        private string name;
+        private string className;
+        private long time;
+
+        internal string Name { get; private set;}
+
+        internal string ClassName { get; private set; }
+
+        internal long Time { get; set; }
+        
+        internal List<MethodResult> InnerMethods
+        {
+            get
+            {
+                if (innerMethods == null)
+                {
+                    innerMethods = new List<MethodResult>();
+                }
+                return innerMethods;
+            }
+        }
+
+        internal void AddInnerMethod(MethodResult method)
+        {
+            innerMethods.Add(method);
+        }
+
+        internal MethodResult(string methodName , string methodClassName)
+        {
+            name = methodName;
+            className = methodClassName;
+        }
     }
 }
