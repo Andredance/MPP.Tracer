c5eed0248b705fcea4ff58566b4624ac18e08628
andredance <andrey99.09@gmail.com>
2018-09-18 20:11:13 +0300

    implement ThreadTracer class

--- Tracer/Tracer/ThreadTracer.cs
+++ Tracer/Tracer/ThreadTracer.cs
@@ -3,11 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace Tracer
 {
-    public class ThreadTracer
+    internal class ThreadTracer
     {
+        private int threadId;
+        private Stack<MethodTracer> methodsInThread;
+        private List<MethodTracer> tracedMethods;
+
+        internal int ThreadId { get; private set; }
+        
+        internal long Time
+        {
+            get
+            {
+                long t = 0;
+                tracedMethods.ForEach((item) => t += item.Time);
+                return t;                
+            }
+        }
+
+        internal Stack<MethodTracer> MethodsInThread
+        {
+            get
+            {
+                if (methodsInThread == null)
+                {
+                    methodsInThread = new Stack<MethodTracer>();
+                }
+                return methodsInThread;
+            }
+        }
+
+        internal List<MethodTracer> TracedMethods
+        {
+            get
+            {
+                if (tracedMethods == null)
+                {
+                    tracedMethods = new List<MethodTracer>();
+                }
+                return tracedMethods;
+            }
+        }
+
+        private void AddMethod(MethodTracer method)
+        {
+            if (MethodsInThread.Count > 0)
+            {
+                MethodsInThread.Peek().AddInnerMethod(method);         
+            } else
+            {
+                MethodsInThread.Push(method);
+            }
+        }
+
+        internal void StartTrace(MethodTracer method)
+        {
+            AddMethod(method);
+            method.StartTrace();
+        }
+
+        internal void StopTrace()
+        {
+            if (MethodsInThread.Count == 0)
+            {
+                throw new InvalidOperationException("Can't stop tracing method that doesn't exist");
+            }
+            MethodTracer popedMethod = MethodsInThread.Pop();
+            popedMethod.StopTrace();
+            TracedMethods.Add(popedMethod);
+        }
+
+        internal ThreadTracer(int id)
+        {
+            ThreadId = id;
+        }
+
 
     }
 }
