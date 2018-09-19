29cf5e07f1f0015794fc04f0779e91e6969bd918
andredance <andrey99.09@gmail.com>
2018-09-19 10:42:34 +0300

    first implementation of TraceResult class

--- Tracer/Tracer/TraceResult.cs
+++ Tracer/Tracer/TraceResult.cs
@@ -1,6 +1,47 @@
-﻿namespace Tracer
+﻿using System.Collections.Concurrent;
+
+namespace Tracer
 {
     public class TraceResult
     {
+        private ConcurrentDictionary<int, ThreadTracer> threadResults;
+
+        internal ConcurrentDictionary<int, ThreadTracer> ThreadResults
+        {
+            get
+            {
+                if (threadResults == null)
+                {
+                    threadResults = new ConcurrentDictionary<int, ThreadTracer>();
+                }
+                return threadResults;
+            }
+        }
+
+        internal TraceResult() { }
+
+        internal bool AddThreadResult(int id)
+        {
+            ThreadTracer threadTracer;
+            if (ThreadResults.TryGetValue(id, out threadTracer)) 
+            {
+                return false;
+            }
+            threadTracer = new ThreadTracer(id);
+            ThreadResults[id] = threadTracer;
+            return true;       
+        }
+
+        internal ThreadTracer GetThreadResult(int id)
+        {
+            ThreadTracer threadTracer;
+            if (ThreadResults.TryGetValue(id, out threadTracer))
+            {
+                return threadTracer;
+            }
+            return null;    
+        }
+
+
     }
 }
