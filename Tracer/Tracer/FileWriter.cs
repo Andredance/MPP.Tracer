8641b99ca13f9a75cd6f48b6a1458b141683ff12
andredance <andrey99.09@gmail.com>
2018-09-20 11:36:44 +0300

    added FileWriter class for File output

---
+++ Tracer/Tracer/FileWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class FileWriter : IWriter
+    {
+        private string fileName;
+        public FileWriter(string fileN)
+        {
+            fileName = fileN;
+        }
+        public void Write(TraceResult traceResult, ISerializer serializer)
+        {
+            using (FileStream fileStream = new FileStream(fileName, FileMode.Append))
+            {
+                serializer.SerializeResult(traceResult, fileStream);
+            }
+        }
+    }
+}
