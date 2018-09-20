1aaeb26c52062286c715f6ee0ac53002ca559ef7
andredance <andrey99.09@gmail.com>
2018-09-20 11:42:20 +0300

    realized XML serializator class

---
+++ Tracer/Tracer/XmlSerializator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Tracer
+{
+    public class XmlSerializator: ISerializer
+    {
+        protected readonly XmlWriterSettings xmlWriterSettings;
+        protected readonly DataContractSerializer xmlSerializer;
+
+        public void SerializeResult(TraceResult traceResult, Stream stream)
+        {
+            using (XmlWriter xmlWriter = XmlWriter.Create(stream, xmlWriterSettings))
+            {
+                xmlSerializer.WriteObject(xmlWriter, traceResult);
+            }
+        }
+
+        public XmlSerializator()
+        {
+            xmlWriterSettings = new XmlWriterSettings
+            {
+                Indent = true
+            };
+            xmlSerializer = new DataContractSerializer(typeof(TraceResult));
+        }
+    }
+}
