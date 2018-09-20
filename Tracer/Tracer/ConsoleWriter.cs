54a67dcb345baae45fe4b3eca7646a72c114da76
andredance <andrey99.09@gmail.com>
2018-09-20 11:36:27 +0300

    added ConsoleWriter class, for console output

---
+++ Tracer/Tracer/ConsoleWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class ConsoleWriter : IWriter
+    {
+        public void Write(TraceResult traceResult, ISerializer serializer)
+        {
+            using (Stream consoleOutPutStream = Console.OpenStandardOutput())
+            {
+                serializer.SerializeResult(traceResult, consoleOutPutStream);
+            }
+        }
+    }
+}
