907c22518fe31c4d3a45fd37c96ffdf0b7779cd9
andredance <andrey99.09@gmail.com>
2018-09-20 11:02:06 +0300

    added IWritter interface

---
+++ Tracer/Tracer/IWritter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public interface IWritter
+    {
+        void Write(TraceResult traceResult, ISerializer serializer);
+    }
+}
