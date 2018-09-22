95b94252fb5ed9c39e37a806fe566b0f5234af85
andredance <andrey99.09@gmail.com>
2018-09-22 18:59:45 +0300

    added console example solution

---
+++ Tracer/TracerConsoleExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using Tracer;
+
+namespace TracerConsoleExample
+{
+    class Program
+    {
+        private static ITracer tracer;
+
+        internal static void AnotherSImpleMethod()
+        {
+            tracer.StartTrace();
+            Thread.Sleep(100);
+            tracer.StopTrace();
+        }
+
+        internal static void SimpleMethod()
+        {
+            tracer.StartTrace();
+            AnotherSImpleMethod();
+            Thread.Sleep(100);
+            tracer.StopTrace();
+        }
+
+        static void Main(string[] args)
+        {
+            tracer = new Tracer.Tracer();
+            SimpleMethod();
+            TraceResult traceResult = tracer.GetTraceResult();
+            XmlSerializator xml = new XmlSerializator();
+            ConsoleWriter cw = new ConsoleWriter();
+            cw.Write(traceResult, xml);
+     
+            FileWriter fw = new FileWriter("C:\\Users\\andre\\Desktop\\json.json");
+            fw.Write(traceResult, xml);
+            Console.ReadKey();
+        }
+    }
+}
